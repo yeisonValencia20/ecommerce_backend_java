0c74c80479ad97c75e3eead4bf19cd66650a787e
YEISON VALENCIA GOMEZ <yeison.vg@outlook.com>
2023-03-22 21:33:49 -0500

    Se crea el repository y las clases services de Producto, asi como su controlador con su metodo show para mostrar la vista

---
+++ src/main/java/com/curso/ecommerce/springecommerce/service/ProductoService.java
@@ -0,0 +1,12 @@
+package com.curso.ecommerce.springecommerce.service;
+
+import java.util.Optional;
+
+import com.curso.ecommerce.springecommerce.model.Producto;
+
+public interface ProductoService {
+    public Producto save( Producto producto );
+    public Optional<Producto> get( Integer id );
+    public void update( Producto producto );
+    public void delete( Integer id );
+}
