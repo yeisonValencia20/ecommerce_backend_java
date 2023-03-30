8be227e11c2ec35a5bb2774ad84c0bc2e70a038a
YEISON VALENCIA GOMEZ <yeison.vg@outlook.com>
2023-03-29 21:09:39 -0500

    Se crea la vista Home y su controlador para listar los productos a los usuarios

---
+++ src/main/java/com/curso/ecommerce/springecommerce/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.curso.ecommerce.springecommerce.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.curso.ecommerce.springecommerce.service.ProductoService;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @Autowired
+    private ProductoService productoService;
+    
+    @GetMapping("")
+    public String home(Model model) {
+        model.addAttribute("productos", productoService.findAll());
+        return "usuario/home";
+    }
+}
