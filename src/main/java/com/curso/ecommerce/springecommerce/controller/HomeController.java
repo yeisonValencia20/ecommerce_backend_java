e002a29a105df17a01109666923279ea7130c343
YEISON VALENCIA GOMEZ <yeison.vg@outlook.com>
2023-03-29 21:31:06 -0500

    mostrar la vista de detalles producto

--- src/main/java/com/curso/ecommerce/springecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/springecommerce/controller/HomeController.java
@@ -1,16 +1,20 @@
 package com.curso.ecommerce.springecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.springecommerce.service.ProductoService;
-
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private ProductoService productoService;
@@ -19,5 +23,11 @@ public class HomeController {
     public String home(Model model) {
         model.addAttribute("productos", productoService.findAll());
         return "usuario/home";
+    }
+
+    @GetMapping("productohome/{id}")
+    public String verProducto(@PathVariable Integer id) {
+        log.info("id producto enviado como parametro {}", id);
+        return "usuario/productoHome";
     }
 }
