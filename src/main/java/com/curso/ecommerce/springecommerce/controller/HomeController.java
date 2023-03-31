f1f4d1a55577dd36c09f4f2e7c295f151e62bc55
YEISON VALENCIA GOMEZ <yeison.vg@outlook.com>
2023-03-30 20:25:12 -0500

    Se crea el controlador y la vista del carrito de compras

--- src/main/java/com/curso/ecommerce/springecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/springecommerce/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.springecommerce.model.Producto;
@@ -33,5 +34,10 @@ public class HomeController {
 
         model.addAttribute("producto", producto);
         return "usuario/productoHome";
+    }
+
+    @PostMapping("cart")
+    public String addCart() {
+        return "usuario/carrito";
     }
 }
