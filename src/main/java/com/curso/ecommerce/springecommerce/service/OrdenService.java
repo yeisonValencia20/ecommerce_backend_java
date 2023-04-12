fdb7a70640b0c9e534342e4bc3943840591398e5
YEISON VALENCIA GOMEZ <yeison.vg@outlook.com>
2023-04-11 21:21:08 -0500

    Se crea el metodo para generar el numero de la orden

--- src/main/java/com/curso/ecommerce/springecommerce/service/OrdenService.java
+++ src/main/java/com/curso/ecommerce/springecommerce/service/OrdenService.java
@@ -1,7 +1,12 @@
 package com.curso.ecommerce.springecommerce.service;
 
+import java.util.List;
+
 import com.curso.ecommerce.springecommerce.model.Orden;
 
 public interface OrdenService {
+
     public Orden save(Orden orden);
+
+    public List<Orden> findAll();
 }
--- src/main/java/com/curso/ecommerce/springecommerce/service/OrdenServiceImpl.java
+++ src/main/java/com/curso/ecommerce/springecommerce/service/OrdenServiceImpl.java
@@ -1,5 +1,8 @@
 package com.curso.ecommerce.springecommerce.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +19,37 @@ public class OrdenServiceImpl implements OrdenService{
     public Orden save(Orden orden) {
        return ordenRepository.save(orden);
     }
+
+    @Override
+    public List<Orden> findAll() {
+        return ordenRepository.findAll();
+    }
     
+    public String generarNumeroOrden() {
+        int numero = 0;
+        List<Orden> ordenes = findAll();
+
+        List<Integer> numeros = new ArrayList<Integer>();
+
+        ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+
+        if (ordenes.isEmpty()) {
+            numero = 1;
+        }
+        else {
+            numero = numeros.stream().max(Integer::compare).get();
+            numero++;
+        }
+
+        return concatenarNumero(numero);
+    }
+
+    public String concatenarNumero(Integer numero) {
+        String numeroString = Integer.toString(numero);
+
+        String zero = "0";
+        String zeros = zero.repeat(10 - numeroString.length());
+
+        return zeros + numeroString;
+    }
 }
