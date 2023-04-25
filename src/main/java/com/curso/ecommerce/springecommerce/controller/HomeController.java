7c271e420bb2430593eb0ab0b2281ce4e1493a2d
YEISON VALENCIA GOMEZ <yeison.vg@outlook.com>
2023-04-24 20:47:43 -0500

    Se crea el endpoint para buscar productos

--- src/main/java/com/curso/ecommerce/springecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/springecommerce/controller/HomeController.java
@@ -153,4 +153,13 @@ public class HomeController {
         
         return "redirect:/";
     }
+
+    @PostMapping("search")
+    public String buscarProducto(@RequestParam String nombre, Model model) {
+        log.info("Nombre del producto: {}", nombre);
+        List<Producto> productos = productoService.findAll().stream().filter(p -> p.getNombre().contains(nombre)).toList();
+        model.addAttribute("productos", productos);
+        
+        return "usuario/home";
+    }
 }
